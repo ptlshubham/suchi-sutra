2c17aadea1aa1819a8faf80fd6253ed8618c0667
Shubham Patel <ptlshubham@gmail.com>
2023-06-29 17:28:45 +0530

    Theme

---
+++ assets/phpmailer/vendor/FormGuide/PHPFormValidator/src/ValidatorsList.php
@@ -0,0 +1,12 @@
+<?php
+return 
+[
+	'email'  => ['message' => '%field% must be a valid email Address
+'],
+	'maxlen' => ['message' => "%field% length shouldn't exceed  %constraint%"],
+	'minlen' => ['message' => "%field% should have minlength of %constraint%"],
+	'alphabetic' => ['message' => "%field% shouldn't contain non-alphabetic characters"],
+	'alphanumeric' => ['message' => "%field% shouldn't contain non-alpha-numeric characters"],
+	'alphabetic_space' => ['message' => "%field% shouldn't contain non-alphabetic characters"],
+	'alphanumeric_space' => ['message' => "%field% shouldn't contain non-alpha-numeric characters"],	
+];
